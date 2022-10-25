baba358462219817d2ec39836c2b2126667da807
Paul Reichmuth <p.n.reichmuth@gmail.com>
2022-10-25 16:10:40 +0200

    added interval

--- new-windsensor.ino
+++ new-windsensor.ino
@@ -1,21 +1,37 @@
-float wind = 0; 
-int sensorPin = A4; 
-int sensorwert; 
-int t = 100; 
+#include <senseBoxIO.h>
+
+#define MEASUREMENT_INTERVAL_SEC 10 //Change rate of measurement here
+
+const int num_measurements = (60*10)/MEASUREMENT_INTERVAL_SEC;
+
+int measurements[num_measurements];
 
 void setup()
+
 {
-  Serial.begin(9600); 
+    Serial.begin(9600);
+    senseBoxIO.powerAll();
+    senseBoxIO.statusGreen();
 }
 
 void loop()
 {
-  sensorwert = analogRead(sensorPin); 
-  Serial.println(sensorwert);
-  float volt = 3.3*((float)sensorwert)/1024;
-  Serial.println(volt);
-  delay(t); 
-  wind = volt*25;
-  Serial.print("Windgeschwindigkeit: "); 
-  Serial.println(wind); 
+    Serial.print("Windgeschwindigkeit: ");
+    Serial.println(getWindspeed(A1));
+    delay(100);
+}
+
+float getWindspeed(int sensorPin)
+{
+    float wind = 0;
+    int sensorwert;
+    //int t = 100;
+    sensorwert = analogRead(sensorPin);
+    Serial.println(sensorwert);
+    float volt = 3.3 * ((float)sensorwert) / 1024;
+    Serial.println(volt);
+    //delay(t);
+    wind = volt * 25;
+
+    return wind;
 }
