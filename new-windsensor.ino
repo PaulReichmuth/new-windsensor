8d7f4159bb6d1b40849e4d055a9649f0d56e5eb2
Paul Reichmuth <p.n.reichmuth@gmail.com>
2022-10-25 17:06:24 +0200

    implement gps

--- new-windsensor.ino
+++ new-windsensor.ino
@@ -22,14 +22,14 @@
 // This object parses received characters
 //   into the gps.fix() data structure
 
-static NMEAGPS  gps;
+static NMEAGPS gps;
 
 //------------------------------------------------------------
 //  Define a set of GPS fix information.  It will
 //  hold on to the various pieces as they are received from
 //  an RMC sentence.  It can be used anywhere in your sketch.
 
-static gps_fix  fix;
+static gps_fix fix;
 
 //----------------------------------------------------------------
 //  This function gets called about once per second, during the GPS
@@ -39,9 +39,9 @@ static gps_fix  fix;
 //  By doing the "hard" work during the quiet time, the CPU can get back to
 //  reading the GPS chars as they come in, so that no chars are lost.
 
-#define MEASUREMENT_INTERVAL_SEC 10 //Change rate of measurement here
+#define MEASUREMENT_INTERVAL_SEC 10 // Change rate of measurement here
 
-const int num_measurements = (60*10)/MEASUREMENT_INTERVAL_SEC; //number of measurements to get in 10 Minutes
+const int num_measurements = (60 * 10) / MEASUREMENT_INTERVAL_SEC; // number of measurements to get in 10 Minutes
 
 int measurements_taken = 0;
 
@@ -51,69 +51,84 @@ uint8_t nextSecond = 0;
 
 static void doSomeWork()
 {
-  // Print all the things!
+    // Print all the things!
 
-  trace_all( DEBUG_PORT, gps, fix );
+    trace_all(DEBUG_PORT, gps, fix);
 
-  //Keep calculating average
+    // Keep calculating average
 
-  if(fix.dateTime.seconds >= nextSecond){
-    measurements[measurements_taken] = getWindspeed(A1);
-    Serial.print("Took Sample Num.: ");
-    Serial.println(i);
-    Serial.print("Windgeschwindigkeit grade: ");
-    Serial.println(measurements[i]);
-    nextSecond = fix.dateTime.seconds + MEASUREMENT_INTERVAL_SEC;
-    measurements_taken ++;
-  }
-  if(measurements_taken == num_measurements){
-    //print avg
-    Serial.print("Durchschnitl. Windgeschwindigkeit: ");
-    Serial.println(average(measurements,num_measurements));
-    measurements_taken = 0;
-  }
-}
-
-void setup()
-
-{
-    Serial.begin(9600);
-    while(!Serial){
-        senseBoxIO.statusRed();
+    if (fix.dateTime.seconds >= nextSecond)
+    {
+        measurements[measurements_taken] = getWindspeed(A1);
+        DEBUG_PORT.print("Took Sample Num.: ");
+        DEBUG_PORT.println(i);
+        DEBUG_PORT.print("Windgeschwindigkeit grade: ");
+        DEBUG_PORT.println(measurements[i]);
+        nextSecond = fix.dateTime.seconds + MEASUREMENT_INTERVAL_SEC;
+        measurements_taken++;
     }
-    senseBoxIO.statusNone();
-    senseBoxIO.powerAll();
-    senseBoxIO.statusGreen();
-    Serial.println("Begin.");
-    Serial.print("Taking ");
-    Serial.print(num_measurements);
-    Serial.println("Samples");
+    if (measurements_taken == num_measurements)
+    {
+        // print avg
+        DEBUG_PORT.print("Durchschnitl. Windgeschwindigkeit: ");
+        DEBUG_PORT.println(average(measurements, num_measurements));
+        measurements_taken = 0;
+    }
 }
 
-void loop()
+static void GPSloop()
 {
-    
+    while (gps.available(gpsPort))
+    {
+        fix = gps.read();
+        doSomeWork();
+    }
 }
 
 float getWindspeed(int sensorPin)
 {
     float wind = 0;
     int sensorwert;
-    //int t = 100;
+    // int t = 100;
     sensorwert = analogRead(sensorPin);
-    //Serial.println(sensorwert);
+    // DEBUG_PORT.println(sensorwert);
     float volt = 3.3 * ((float)sensorwert) / 1024;
-    //Serial.println(volt);
-    //delay(t);
+    // DEBUG_PORT.println(volt);
+    // delay(t);
     wind = volt * 25;
 
     return wind;
 }
 
-float average (float * array, int len)  // assuming array is int.
+float average(float *array, int len) // assuming array is int.
 {
-  long sum = 0L ;  // sum will be larger than an item, long for safety.
-  for (int i = 0 ; i < len ; i++)
-    sum += array [i] ;
-  return  ((float) sum) / len ;  // average will be fractional, so float may be appropriate.
+    long sum = 0L; // sum will be larger than an item, long for safety.
+    for (int i = 0; i < len; i++)
+        sum += array[i];
+    return ((float)sum) / len; // average will be fractional, so float may be appropriate.
+}
+
+void setup()
+
+{
+    senseBoxIO.powerAll();
+    SD.begin(28);
+    DEBUG_PORT.begin(9600);
+    DEBUG_PORT.print(F("NMEA.INO: started\n"));
+    DEBUG_PORT.print(F("  fix object size = "));
+    DEBUG_PORT.println(sizeof(gps.fix()));
+    DEBUG_PORT.print(F("  gps object size = "));
+    DEBUG_PORT.println(sizeof(gps));
+    DEBUG_PORT.println(F("Looking for GPS device on " GPS_PORT_NAME));
+    DEBUG_PORT.flush();
+    gpsPort.begin( 9600 );
+    DEBUG_PORT.println("Begin.");
+    DEBUG_PORT.print("Taking ");
+    DEBUG_PORT.print(num_measurements);
+    DEBUG_PORT.println("Samples");
+}
+
+void loop()
+{
+    GPSloop();
 }
